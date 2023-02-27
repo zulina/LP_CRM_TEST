764c079d6a82813db5a973699f2d6ab92bf33d26
MacBook <macbook@MacBook-Pro-MacBook.local>
2023-02-27 17:43:53 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
 import React from 'react';
 import Select from 'react-select'
 import './App.css';
+import './Switch.css';
+
+let newItem = false;
+let deleteItem = false;
+let counter = 3;
 
 const data = [
   {pos: 1, status: false, product: 'XXXX-', id: '1', name: 'blue', key: 1, active: false},
-  {pos: 2, status: false, product: 'XXXX-', id: '2', name: 'red', key: 2, active: false},
+  {pos: 2, status: true, product: 'XXXX-', id: '2', name: 'red', key: 2, active: false},
   {pos: 3, status: false, product: 'XXXX-', id: '3', name: 'green', key: 3, active: false}
 ];
 
-const nameOptions = [
-  {value: 'blue', label: 'blue'},
-  {value: 'red', label: 'red'},
-  {value: 'green', label: 'green'}
-];
+let nameOptionsDone = false;
+let nameOptions = [];
 
 let filterOptions = [];
 
 const getItemById = (id) => {
-  let foundItem = undefined;
-  data.forEach(item => {
-    if (item.id === id) {
-      foundItem = item;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
     }
-  });
-  return foundItem;
+  }
+  return undefined;
 }
 
 const getItemByPos = (pos) => {
-  let foundItem = undefined;
-  data.forEach(item => {
-    if (item.pos === pos) {
-      foundItem = item;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].pos == pos) {
+      return data[i];
     }
-  });
-  return foundItem;
+  }
+  return undefined;
 }
 
-let newItem = false;
+const getItemIndex = (pos) => {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].pos == pos) {
+      return i;
+    }
+  }
+  return undefined;
+}
 
 function ImgOption(name) {
   return (
@@ -54,7 +61,7 @@ while (num < 5) {
   imgOptions.push({ value: name, label: ImgOption(name) });
 }
 
-function TableItem({pos, status, product, id, name, active}) {
+function TableItem({pos, status, product, id, name}) {
 
   const [state, setState] = React.useState({
     itemStatus: status,
@@ -64,11 +71,7 @@ function TableItem({pos, status, product, id, name, active}) {
   });
 
   const onClickStatus = (event) => {
-    data.forEach((item) => {
-      if (item.id === id) {
-        item.status = !item.status;
-      }
-    });
+    getItemByPos(pos).status = !state.itemStatus;
     setState((prevState) => ({
       ...prevState,
       itemStatus: !state.itemStatus
@@ -98,24 +101,20 @@ function TableItem({pos, status, product, id, name, active}) {
   }
 
   const onClickItem = (e) => {
-    // console.log(typeof(e.target.className));
-    // console.log(e.target.className);
-    // console.log(e.target.tagName);
     if (e.target && ((typeof(e.target.className) !== 'string') || (e.target.tagName === 'DIV') || (e.target.tagName === 'IMG') ||
-      e.target.className.includes('item_input') || (e.target.type === "checkbox"))) {
+      e.target.className.includes('item_input') || e.target.className.includes('item_delete') || (e.target.type === "checkbox") ||
+      e.target.className.includes('react-switch-label') || e.target.className.includes('react-switch-button'))) {
         return null;
     }
-    data.forEach((item) => {
-      if (item.id === id) {
-        item.active = !item.active;
-      }
-    });
+    console.log('onClickItem');
     if (state.itemClassName.includes('item_active')) {
+      getItemByPos(pos).active = false;
       setState((prevState) => ({
         ...prevState,
         itemClassName: state.itemClassName.replace(' item_active', '')
       }));
     } else {
+      getItemByPos(pos).active = true;
       setState((prevState) => ({
         ...prevState,
         itemClassName: state.itemClassName + ' item_active'
@@ -173,16 +172,16 @@ function TableItem({pos, status, product, id, name, active}) {
     if (newItem) {
       newItem = false;
 
+      // добавление новой строки
+      counter++;
       data.unshift({
         pos: pos,
         status: state.itemStatus, 
         product: 'XXXX-', 
         id: document.getElementById('id').value, 
         name: document.getElementById('name').value, 
-        key: data.length + 1, 
-        active: state.itemClassName.includes('item_active')});
-      nameOptions.unshift({value: document.getElementById('name').value, label: document.getElementById('name').value});
-      // console.log(data[0]);
+        key: counter, 
+        active: state.itemClassName.includes('item_active')});    
 
       if (state.focusName) {
         setState((prevState) => ({
@@ -197,9 +196,32 @@ function TableItem({pos, status, product, id, name, active}) {
     e.target.blur();
   }
 
+  const onClickDelete = () => {
+    // удаление элемента массива
+    deleteItem = true;
+    data.splice(getItemIndex(pos), 1);
+    // data.forEach(item => { console.log(item); });
+  }
+
+  // <input type="checkbox" value={state.itemStatus} name="status" onClick={onClickStatus} />
   return (
     <tr className={state.itemClassName} onClick={onClickItem}>
-      <td><input type="checkbox" value={state.itemStatus} name="status" onClick={onClickStatus} /></td>
+      <td>
+          <input
+            className="react-switch-checkbox"
+            id={`react-switch-${pos}`}
+            type="checkbox"
+            value={state.itemStatus}
+            defaultChecked={status}
+            onClick={onClickStatus}
+          />
+          <label
+            className="react-switch-label"
+            htmlFor={`react-switch-${pos}`}
+          >
+            <span className="react-switch-button" />
+          </label>
+      </td> 
       <td>{product}</td>
       <td><input className='item_input' type="text" maxLength='3' defaultValue={id} 
         onKeyDown={onChangeId} autoFocus={state.focusId} onBlur={onSubmitId} id='id' /></td>
@@ -208,7 +230,8 @@ function TableItem({pos, status, product, id, name, active}) {
         <input className='item_input' type="text" defaultValue={name} 
         onKeyDown={onChangeName} autoFocus={state.focusName} onBlur={onSubmitName} id='name' />
       </td>
-      <td><button className='item_delete' title='Удалить строку' onMouseOver={onMouseOverDelete} onMouseOut={onMouseOutDelete}>x</button></td>
+      <td><button className='item_delete' title='Удалить строку' onMouseOver={onMouseOverDelete} onMouseOut={onMouseOutDelete}
+        onClick={onClickDelete}>x</button></td>
     </tr>
   );
 }
@@ -216,20 +239,20 @@ function TableItem({pos, status, product, id, name, active}) {
 function App() {
 
   const [state, setState] = React.useState({
-    btnClassName: 'btn_delete_all',
+    btnClassName: 'btn_delete_selected',
     addItem: newItem,
+    delItem: deleteItem,
     filterOn: false
   });
 
   let items = [];
   if (state.filterOn) {
-    const dataFilter = data.filter(item => {
-      let i = 0;
-      while (i < filterOptions.length) {
-        if (item.name === filterOptions[i].value) {
+
+    const dataFilter = data.filter(item => {      
+      for (let i = 0; i < filterOptions.length; i++) {
+        if (item.name === filterOptions[i]) {
           return true;
-        }
-        i++;
+        }       
       }
     });
 
@@ -238,35 +261,49 @@ function App() {
           <TableItem {...item} />
       )
     });
+
   } else {
+
     items = data.map(item => {
       return (
           <TableItem {...item} />
       )
     });
+
   }
+
   if (newItem) {
-    const item = {pos: data.length + 1, status: false, product: 'XXXX-', id: '', name: '', key: data.length + 1, active: false};
+    const item = {pos: counter + 1, status: false, product: 'XXXX-', id: '', name: '', key: counter + 1, active: false};
     items.unshift( <TableItem {...item} /> );
   }
 
-  const onChangeTable = () => {
-    // data.forEach(item => { console.log(item); });
-    if (data.some((item) => item.active)) {
-      if (!state.btnClassName.includes('btn_delete_all_active')) {
-        setState((prevState) => ({
-          ...prevState,
-          btnClassName: 'btn_delete_all_active',
-          addItem: newItem
-        }))
-      }
+  const onClickTable = (event) => {
+    if (deleteItem) {
+      setState((prevState) => ({
+        ...prevState,
+        delItem: deleteItem
+      }));
+      deleteItem = false;
+      setState((prevState) => ({
+        ...prevState,
+        delItem: deleteItem
+      }));
     } else {
-      if (state.btnClassName.includes('btn_delete_all_active')) {
-        setState((prevState) => ({
-          ...prevState,
-          btnClassName: 'btn_delete_all',
-          addItem: newItem
-        }))
+      // data.forEach(item => { console.log(item); });
+      if (data.some(item => item.active)) {
+        if (!state.btnClassName.includes('btn_delete_selected_active')) {
+          setState((prevState) => ({
+            ...prevState,
+            btnClassName: 'btn_delete_selected_active'
+          }))
+        }
+      } else {
+        if (state.btnClassName.includes('btn_delete_selected_active')) {
+          setState((prevState) => ({
+            ...prevState,
+            btnClassName: 'btn_delete_selected'
+          }))
+        }
       }
     }
   }
@@ -281,14 +318,32 @@ function App() {
     }
   }
 
-  const onMouseOverSelectName = (event) => {
-    document.getElementById('selectName').click();
+  const onMouseOverSelectName = () => {
+    if (!nameOptionsDone) {    
+      // перезаполняем опции
+      nameOptions = [{value: 'All', label: 'Все'}];
+      data.forEach(item => {
+        if (!nameOptions.some(op => (op.value === item.name))) {
+          nameOptions.push({value: item.name, label: item.name});
+        }
+      });
+      setState((prevState) => ({
+        ...prevState
+      }));
+      nameOptionsDone = true;
+    }
+    //document.getElementById('select_name').click();
+  }
+
+  const onMouseLeaveSelectName = () => {
+    nameOptionsDone = false;
   }
 
   const onChangeSelectName = (selectedOptions) => {
-    // console.log(selectedOptions);
-    filterOptions = selectedOptions;
-    if (selectedOptions.length > 0) {
+    filterOptions = [];
+    // если не выбрано Все
+    if ((selectedOptions.length > 0) && !selectedOptions.some(item => (item.value === nameOptions[0].value))) {
+      selectedOptions.forEach(item => { filterOptions.push(item.value) });
       setState((prevState) => ({
         ...prevState,
         filterOn: true  
@@ -301,9 +356,27 @@ function App() {
     }
   }
 
+  const onClickDeleteSelected = () => {
+
+    // удаление элементов массива
+    let length = data.length;
+    for (let index=0; index<length; index++) {
+      if (data[index].active) {
+        data.splice(index, 1);
+        index--;
+        length--;
+      }
+    }
+  
+    setState((prevState) => ({
+      ...prevState,
+      btnClassName: 'btn_delete_selected'
+    }));
+  }
+
   return (
     <div className="App">
-      <table className='table' onClick={onChangeTable}>
+      <table className='table' id='table' onClick={onClickTable}>
         <thead>
           <tr>
             <th>Статус</th>
@@ -316,9 +389,11 @@ function App() {
             <th><Select placeholder='' /></th>
             <th><Select placeholder='' /></th>
             <th><Select placeholder='' /></th>
-            <th><div onMouseOver={onMouseOverSelectName}><Select placeholder='' name="selectName" id="selectName" 
-              className="select_name" options={nameOptions} isMulti onChange={onChangeSelectName}></Select></div></th>
-            <th><button className={state.btnClassName}>x</button></th>
+            <th><div onMouseOver={onMouseOverSelectName} onMouseLeave={onMouseLeaveSelectName}>
+              <Select placeholder='' name="select_name" id="select_name" className="select_name" 
+              options={nameOptions} isMulti hideSelectedOptions={false} onChange={onChangeSelectName}></Select>
+            </div></th>
+            <th><button className={state.btnClassName} id='delete_selected' onClick={onClickDeleteSelected}>x</button></th>
           </tr>
         </thead>
         <tbody>
